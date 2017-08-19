64aae3ee53ec2405cde3dc88876d96dd500b5fdc
Jens Theisen <jens.theisen@gmail.com>
2017-08-19 17:49:46 +0200

    wip

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,22 @@
 
+/*
+
+We do *only* the monadic version at first. This is in fact easier (as the monadic expressions reflect
+complete sql query units) and it's what I seek to prove possible anyway.
+
+We now have two sets of types so far:
+- Typed Scalar/SqlSet for fluid syntax and
+- Untyped Expressions representing an intermediate form of the query
+
+The expression already shows the structure the query will have (the number of subqueries, for example).
+It does not yet, however, contain the following:
+- There are no aliases in the expressions. Those will be given in the final render step.
+- Implicit joins necessitated through accessing a scalar navigational property.
+- Implicit applies?
+
+*/
+
+
 
 class Entity {
     name: string
@@ -12,16 +30,11 @@ class City {
 
 // aliased set/view/cte or query/subquery
 class AliasedSetExpression {
-    alias: string
-
     set: SetExpression
 }
 
 // table/view/cte
 class SetExpression {
-    name: string
-
-    fields: string[] // column aliases
 }
 
 class QueriedSetExpression extends SetExpression {
@@ -52,7 +65,9 @@ class SelectExpression {
 
     orderby: OrderByExpression
 
-    select: ScalarExpression[]
+    select: ScalarExpression
+
+    distinct: boolean
 }
 
 class JoinExpression {
@@ -126,7 +141,13 @@ class NotExpression extends PredicateExpression {
     operand: PredicateExpression
 }
 
+class ConcreteScalar<T>
+{
+    value: T
 
+    constructor() {
+    }
+}
 
 
 // The point being that this prohibits unwittingly calling inappropriate functions in query expressions.
@@ -157,9 +178,6 @@ var SqlTrue: Predicate;
 class SqlSet<E> {
     //dontuse: E // why is this necessary?
 
-    constructor(private expression: SelectExpression) {
-    }
-
     join<E2>(s2: SqlSet<E2>): JoinStep2<E, E2>
     {
 
@@ -170,17 +188,14 @@ class SqlSet<E> {
 
     }
 
-    map<T>(f: (e: Scalar<E>) => Scalar<T>): SqlSet<T>;
+    // map<T>(f: (e: Scalar<E>) => Scalar<T>): SqlSet<T>;
 
-    group<T>(f: (e: E) => T): GroupStep<E, T>;
-    // shortcut
-    groupby<K>(p: (e: E) => K): SqlSet<Grouping<E, K>>;
+    // group<T>(f: (e: E) => T): GroupStep<E, T>;
+    // // shortcut
+    // groupby<K>(p: (e: E) => K): SqlSet<Grouping<E, K>>;
 }
 
-class ConcreteSqlSet<E> {
-    constructor() {
-
-    }
+class ConcreteSqlSet<E> extends SqlSet<E> {
 }
 
 
@@ -202,7 +217,7 @@ class JoinStep2<E, E2> {
     }
 }
 
-function from<E>(entity: E): SqlSet<E> { throw null; }
+//function from<E>(entity: E): SqlSet<E> { throw null; }
 
 function map<E, T>(source: SqlSet<E>, f: (e: E) => T): SqlSet<T> {
     throw null;
@@ -216,6 +231,7 @@ var myCities: SqlSet<City>;
 var myEntity = new Entity();
 
 
+
 var evaluationStack: {
     expression: SelectExpression
 }[] = [];
@@ -224,39 +240,68 @@ function getCurrentEvaluation() {
     return evaluationStack[evaluationStack.length - 1]
 }
 
-function query<E>(monad: () => E[]) {
+var query: {
+    <E>(monad: () => Scalar<E>): SqlSet<E>
+    <E>(monad: () => E): SqlSet<E>
+}
+= <E>(monad: () => Scalar<E>) => {
     evaluationStack.push({ expression: new SelectExpression() })
     try {
         var result = monad()
 
         var evaluation = getCurrentEvaluation();
 
-        
+        return new ConcreteSqlSet<E>()
     }
     finally {
         evaluationStack.pop();
     }
 }
 
-function from<S>(source: SqlSet<S>) {
-    var evaluation = getCurrentEvaluation();
+declare function scalar<E>(element: E): Scalar<E>;
+declare function scalar<E>(element: Scalar<E>): Scalar<E>;
+
+var x = scalar
+
+// function scalar<E>(element: E): Scalar<E> {
+//     return element as any as Scalar<E>
+// }
+
+
+function makeAliasedSetExpression<T>(source: SqlSet<T>) {
     var aliased = new AliasedSetExpression();
-    aliased.alias = 'some-alias';
-    evaluation.expression.from = source.xxx
+    aliased.set = getSetExpression(source)
+    return aliased
 }
 
-function where<S>(predicate: Predicate) {
-    var evaluation = getCurrentEvaluation()
-    var expression predicate.buildExpression()
-    evaluation.expression.where = expression
+
+function from<S>(source: SqlSet<S>) {
+    var evaluation = getCurrentEvaluation();
+    evaluation.expression.from = makeAliasedSetExpression(source)
+    return new ConcreteScalar<S>() as Scalar<S>;
 }
+
+function join<S>(source: SqlSet<S>, condition: (s: Scalar<S>) => Predicate): Scalar<S>
+{
+    var evaluation = getCurrentEvaluation();
+
+    var joinExpression = new JoinExpression()
+    joinExpression.source = makeAliasedSetExpression(source)
+    joinExpression.kind = 'join'
+    //joinExpression.on = getPredicate(condition)
+
+    evaluation.expression.joins.push(joinExpression)
+
+    return new ConcreteScalar<S>() as Scalar<S>
+}
+
 
 var myQuery = query(() =>
 {
     var x = from(myEntities);
-    var y = join(myCities).on(c => x.city.id.eq(c.id));
+    var y = join(myCities, c => x.city.name.eq(c.name));
 
-    where();
+    var p = { e: x, c: y }
 
-    return [x, y];
+    return p;
 })
